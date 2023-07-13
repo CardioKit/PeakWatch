8c1d7b124565e2fdb3d7ed7a85cdc3a0ed552859
NikitaChar <nikita.charush@gmail.com>
2023-07-13 17:52:36 +0200

    feat: add Grid to ECG chart

--- PeakWatch/Views/ECGDetails/SharedView/ECGChartView.swift
+++ PeakWatch/Views/ECGDetails/SharedView/ECGChartView.swift
@@ -19,33 +19,50 @@ struct ECGChartView<Content: ChartContent>: View {
         ScrollView(.horizontal) {
             Chart {
                 content
-            }.chartXScale(domain: 0...chartRange)
-                .frame(width: CGFloat(chartRange) * 0.5, height: 300)
+            }
+            .chartXScale(domain: 0...chartRange)
+            .frame(width: CGFloat(chartRange) * 0.5, height: 300)
             .chartLegend(.visible)
             .chartXAxis {
-                AxisMarks(values: .stride(by: (self.samplingRate / 5.0))) { value in
-                          if let intValue = value.as(Int.self) {
-                              if intValue % Int(self.samplingRate) == 0  {
-                                  AxisTick(stroke: .init(lineWidth: 1))
-                                      .foregroundStyle(.gray)
-                                  AxisValueLabel() {
-                                      Text("\(intValue / Int(self.samplingRate))s")
-                                  }
-                                  AxisGridLine(stroke: .init(lineWidth: 1))
-                                      .foregroundStyle(.gray)
-                              } else {
-                                  AxisGridLine(stroke: .init(lineWidth: 1))
-                                      .foregroundStyle(.gray.opacity(0.25))
-                              }
-                          }
-                      }
-                  }
+                self.xAxisMarks
+            }
             .chartYAxis {
-                  AxisMarks(values: .automatic(desiredCount: 14)) { value in
-                      AxisGridLine(stroke: .init(lineWidth: 1))
-                          .foregroundStyle(.gray.opacity(0.25))
-                  }
-              }
+                self.yAxisMarks
+            }
+        }
+    }
+    
+    var yAxisMarks: some AxisContent {
+        AxisMarks(values: .automatic(desiredCount: 14)) { value in
+            AxisGridLine(stroke: .init(lineWidth: 1))
+                .foregroundStyle(.gray.opacity(0.25))
+        }
+    }
+    
+    var xAxisMarks: some AxisContent {
+        
+        // The samping rate is the amount of samples per second
+        // We need five lines per second
+        // Each 5th line is a second and we mark the 5th
+        let intervalMss200 = self.samplingRate / 5.0
+        
+        return AxisMarks(values: .stride(by: (intervalMss200))) { value in
+            if let intValue = value.as(Int.self) {
+                if intValue % Int(self.samplingRate) == 0  {
+                    // Thick line
+                    AxisTick(stroke: .init(lineWidth: 1))
+                        .foregroundStyle(.gray)
+                    AxisValueLabel() {
+                        Text("\(intValue / Int(self.samplingRate))s")
+                    }
+                    AxisGridLine(stroke: .init(lineWidth: 1))
+                        .foregroundStyle(.gray)
+                } else {
+                    // Light line
+                    AxisGridLine(stroke: .init(lineWidth: 1))
+                        .foregroundStyle(.gray.opacity(0.25))
+                }
+            }
         }
     }
 }
