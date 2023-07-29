f13c3343a2470fd077e414dbef59dad08416ef25
NikitaChar <nikita.charush@gmail.com>
2023-07-29 15:41:12 +0200

    feat(PerformanceChart): Improve design

--- PeakWatch/Views/ECGDetails/ECGDetailAllAlgorithmsView/ECGAlgorithmPerformanceView.swift
+++ PeakWatch/Views/ECGDetails/ECGDetailAllAlgorithmsView/ECGAlgorithmPerformanceView.swift
@@ -19,6 +19,9 @@ struct ECGAlgorithmPerformanceView: View {
         CGFloat((barWidth + spacing) * Double(algorithmViewModel.qrsResultsByAlgorithm.count))
     }
     
+    let barColor = Color(.red)
+    
+    let xAxisLabel = "Execution time in milliseconds"
     
     var body: some View {
         VStack {
@@ -29,8 +32,35 @@ struct ECGAlgorithmPerformanceView: View {
                         y: .value("Algorithm", qrsResults.algorithm.description),
                         width: MarkDimension(floatLiteral: barWidth)
                     )
+                    .annotation(position: .trailing) {
+                        ExecutionTimeLabel(duration: qrsResults.duration)
+                    }
+                    .foregroundStyle(barColor)
                 }
             }
-        }.frame(height: chartHeight)
+        }
+        .frame(height: chartHeight)
+        .chartXAxisLabel(xAxisLabel)
+        .chartXAxis {
+            AxisMarks { mark in
+                let value = mark.as(Int.self)!
+                AxisGridLine()
+                AxisTick()
+                AxisValueLabel {
+                    Text("\(value) ms")
+                }
+            }
+        }
+    }
+    
+    struct ExecutionTimeLabel: View {
+        let duration: Duration
+        
+        var body: some View {
+            let ms = String(format: "%.2f", duration.inMilliseconds)
+            return Text("\(ms) ms")
+                .foregroundStyle(Color.gray)
+                .font(.system(size: 12, weight: .bold))
+        }
     }
 }
