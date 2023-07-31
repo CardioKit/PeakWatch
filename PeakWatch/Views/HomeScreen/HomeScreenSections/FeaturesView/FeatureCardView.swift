e4c3c216d3c1c8c39d52c926f7b9fd4dad14b049
NumericalMax <max.kapsecker@gmail.com>
2023-07-31 13:39:27 +0200

    Adapted Background

--- PeakWatch/Views/HomeScreen/HomeScreenSections/FeaturesView/FeatureCardView.swift
+++ PeakWatch/Views/HomeScreen/HomeScreenSections/FeaturesView/FeatureCardView.swift
@@ -13,7 +13,7 @@ struct FeatureCardView: View {
     let feature: Feature
     
     var body: some View {
-        CardView(style: .quaternary, padding: 20) {
+        CardView(style: .background, padding: 20) {
             VStack {
                 Image(systemName: feature.icon)
                     .font(.system(size: 30))
