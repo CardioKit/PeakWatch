f8ea63ad9eb018ecab53ede94721de821520f127
NumericalMax <max.kapsecker@gmail.com>
2023-08-01 20:10:52 +0200

    unambiguous language

--- PeakWatch/Views/HomeScreen/HomeScreenSections/GoalView.swift
+++ PeakWatch/Views/HomeScreen/HomeScreenSections/GoalView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct GoalView: View {
     
     
-    let goalTitle = "PeakWatch"
-    let goalDescription = "The objective of PeakWatch is to demonstrate the capabilities of PeakSwift, a Swift package for detecting R peaks in Electrocardiograms."
+    let objectiveTitle = "PeakWatch"
+    let objectiveDescription = "The objective of PeakWatch is to demonstrate the capabilities of PeakSwift, a Swift package for detecting R peaks in Electrocardiograms."
     
     var body: some View {
             VStack {
-                Text(goalTitle).modifier(HeaderViewModifier())
+                Text(objectiveTitle).modifier(HeaderViewModifier())
                 CardView(style: Colors.cardColor.value, padding: 15, cornerRadius: 8) {
-                Text(goalDescription)
+                Text(objectiveDescription)
                 }
         }
     }
