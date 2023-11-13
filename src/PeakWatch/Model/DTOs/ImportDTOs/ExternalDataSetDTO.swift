6adfafe40fd3430087dfd763e3226dc0022039e0
NikitaChar <nikita.charush@gmail.com>
2023-11-13 16:49:01 +0100

    feature(DTOs): Add import DTOs

---
+++ src/PeakWatch/Model/DTOs/ImportDTOs/ExternalDataSetDTO.swift
@@ -0,0 +1,14 @@
+//
+//  ExternalDataSetDTO.swift
+//  PeakWatch
+//
+//  Created by Nikita Charushnikov on 13.11.23.
+//
+
+import Foundation
+
+struct ExternalDataSetDTO: Decodable {
+    
+    let dataset: ECGImportDTO
+    
+}
